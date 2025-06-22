2ac2a7e582bac2f4095e96bcf930b3ed17dd0cc4
thatGui <thatgui@gmail.com>
2025-06-22 18:25:22 +0200

    refactor: Update project references and IRepository interface for consistency and async support

--- Domain/Interfaces/IRepository.cs
+++ Domain/Interfaces/IRepository.cs
@@ -6,11 +6,12 @@ namespace Domain.Interfaces;
 
 public interface IRepository<T> where T : class
 {
-    Task<T?> GetByIdAsync(Guid id);
     Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(Guid id);
+    Task<T> GetByUlnAsync(decimal uln);
     Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
     Task AddAsync(T entity);
     Task AddRangeAsync(IEnumerable<T> entities);
-    void Update(T entity);
-    void Remove(T entity);
+    Task UpdateAsync(T entity);
+    Task RemoveAsync(T entity);
 }
