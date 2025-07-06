06e2425271092adc7e25d3b18978eb0f65a0dbbb
thatGui <thatgui@gmail.com>
2025-07-06 20:04:16 +0100

    feat: Enhance error handling in controllers and add test project setup
    - Update `TransactionsController` and `ApprenticesController` to include detailed error handling for `KeyNotFoundException` and general exceptions.
    - Add `Tests` project to solution with initial setup and references to relevant projects.
    - Upgrade EF Core and Npgsql packages to latest versions.

--- Api/Controllers/ApprenticesController.cs
+++ Api/Controllers/ApprenticesController.cs
@@ -23,81 +23,193 @@ public class ApprenticesController : Controller
     
     [HttpGet("all")]
     [ProducesResponseType(typeof(List<ResponseApprenticeDto>), 200)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
     public async Task<ActionResult<List<ResponseApprenticeDto>>> GetAll()
     {
-        return await _readRepository.GetAllAsync();
+        try
+        {
+            return await _readRepository.GetAllAsync();
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
     
     [HttpGet("{id}")]
     [ProducesResponseType(typeof(ResponseApprenticeDto), 200)]
-    [ProducesResponseType(404)]
-    public async Task<ActionResult<Transaction>> GetById(Guid id)
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
+    public async Task<ActionResult<Apprentice>> GetById(Guid id)
     {
-        var apprentice = await _readRepository.GetByIdAsync(id);
-        if (apprentice == null)
+        try
+        {
+            var apprentice = await _readRepository.GetByIdAsync(id);
+            return Ok(apprentice);
+        }
+        catch (KeyNotFoundException)
         {
             return NotFound();
         }
-
-        return Ok(apprentice);
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
     
     [HttpGet("uln/{uln}")]
-    [ProducesResponseType(typeof(IEnumerable<Apprentice>), 200)]
+    [ProducesResponseType(typeof(ResponseApprenticeDto), 200)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
     public async Task<ActionResult<IEnumerable<Apprentice>>> GetByUlnAsync(decimal uln)
     {
-        var apprentice = await _readRepository.GetByUlnAsync(uln);
-        return Ok(apprentice);
+        try
+        {
+            var apprentice = await _readRepository.GetByUlnAsync(uln);
+            return Ok(apprentice);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
     
     [HttpPost("find")]
     [ProducesResponseType(typeof(List<ResponseApprenticeDto>), 200)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
     public async Task<ActionResult<List<ResponseApprenticeDto>>> Find(
         [FromBody] FindApprentice request)
     {
-        Expression<Func<Apprentice, bool>> predicate = a =>
-            (!request.StartDate.HasValue || a.StartDate >= request.StartDate.Value) &&
-            (!request.EndDate.HasValue || a.EndDate >= request.EndDate.Value) &&
-            (string.IsNullOrEmpty(request.Status) || a.Status == request.Status) &&
-            (!request.Directorate.HasValue || a.Directorate >= request.Directorate.Value) &&
-            (!request.ApprenticeProgram.HasValue || a.ApprenticeProgram >= request.ApprenticeProgram.Value);
+        try
+        {
+            Expression<Func<Apprentice, bool>> predicate = a =>
+                (!request.StartDate.HasValue || a.StartDate >= request.StartDate.Value) &&
+                (!request.EndDate.HasValue || a.EndDate >= request.EndDate.Value) &&
+                (string.IsNullOrEmpty(request.Status) || a.Status == request.Status) &&
+                (!request.Directorate.HasValue || a.Directorate >= request.Directorate.Value) &&
+                (!request.ApprenticeProgram.HasValue || a.ApprenticeProgram >= request.ApprenticeProgram.Value);
 
-        var apprentices = await _readRepository.FindAsync(predicate);
+            var apprentices = await _readRepository.FindAsync(predicate);
 
-        return Ok(apprentices);
+            return Ok(apprentices);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
     
     [HttpPost("create")]
-    [ProducesResponseType(typeof(Apprentice), 201)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
     public async Task<ActionResult<WriteApprenticeDto>> ApprenticeCreate(WriteApprenticeDto apprenticeDto)
     {
-        await _writeRepository.AddAsync(apprenticeDto);
-        return Ok();
+        try
+        {
+            await _writeRepository.AddAsync(apprenticeDto);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
     
     [HttpPost("upload")]
-    [ProducesResponseType(typeof(Apprentice), 201)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
     public async Task<ActionResult<IEnumerable<WriteApprenticeDto>>> ApprenticeUpload(IEnumerable<WriteApprenticeDto> apprenticeDtos)
     {
-        await _writeRepository.UploadAsync(apprenticeDtos);
-        return Ok();
+        try
+        {
+            await _writeRepository.UploadAsync(apprenticeDtos);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
     
     
     [HttpPatch]
-    [ProducesResponseType(typeof(Apprentice), 200)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
     public async Task<ActionResult> UpdateApprentice(Apprentice apprentice)
     {
-        await _writeRepository.UpdateAsync(apprentice);
-        return NoContent();
+        try
+        {
+            await _writeRepository.UpdateAsync(apprentice);
+            return NoContent();
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
     
-    [HttpDelete("{id}")]
-    [ProducesResponseType(typeof(Apprentice), 200)]
+    [HttpDelete("{Id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(statusCode: 400, type: typeof(ValidationProblemDetails))]
     public async Task<IActionResult> DeleteApprentice(Guid id)
     {
-        await _writeRepository.RemoveAsync(id);
-        return NoContent();
+        try
+        {
+            await _writeRepository.RemoveAsync(id);
+            return NoContent();
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Detail = ex.Message
+            });
+        }
     }
 
 }
